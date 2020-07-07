37dd305a019331b0362d1245aaa844dc24893a4c
Dream-By <dzmitry.dzianisau@gmail.com>
2020-07-07 17:34:22 +0300

    Demo version with Firebase

---
+++ app/src/main/java/com/example/firestoretest2020/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.firestoretest2020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firestore = Firebase.firestore
+
+        button.setOnClickListener{
+            val comment = editText.text?.toString()
+
+            if (comment.isNullOrBlank()){
+                return@setOnClickListener
+            }
+
+            firestore.collection("feedback")
+                    .add(hashMapOf(
+                            "comment" to comment
+                    ))
+
+                    .addOnSuccessListener {
+                        Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
+
+                    }
+                    .addOnFailureListener{
+                        Toast.makeText(this,"Failure",Toast.LENGTH_SHORT).show()
+
+                    }
+
+        }
+
+    }
+}
